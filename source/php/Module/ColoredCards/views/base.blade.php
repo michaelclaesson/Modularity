cc87e013e86338d4a2182b6106a8300a44733ed4
Michael Claesson <michael.claesson@consid.se>
2025-04-15 10:26:11 +0200

    Better style structure for colored cards

--- source/php/Module/ColoredCards/views/base.blade.php
+++ source/php/Module/ColoredCards/views/base.blade.php
@@ -1,31 +1,31 @@
-@if ($wrapCards)
+@if ($wrapCards && $wrapperBackgroundColor)
     <div class="{{ $wrapperClass }}">
         <div class="o-container">
             @include('cards')
         </div>
     </div>
-    <style>
-        .{{ $baseClass }}__wrapper {
-            @if ($wrapperBackgroundColor)
-                background: {{ $wrapperBackgroundColor }};
-            @endif
-            position: relative;
-            left: 50%;
-            width: calc(100vw - var(--scrollbar));
-            margin-left: calc(-50vw + calc(var(--scrollbar) / 2));
-        }
-    </style>
 @else
     @include('cards')
 @endif
-<style>
-    @if ($cardStyles)
-        @foreach ($cardStyles as $cardId => $values)
-            .{{ $cardClass }}-{{ $cardId }},
-            .{{ $cardClass }}-{{ $cardId }}:hover {
-                background: {{ $values['background'] }} !important;
-                color: {{ $values['color'] }} !important;
+@if (($wrapCards && $wrapperBackgroundColor) || $cardStyles)
+    <style>
+        @if ($wrapCards && $wrapperBackgroundColor)
+            .{{ $baseClass }}__wrapper {
+                background: {{ $wrapperBackgroundColor }};
+                position: relative;
+                left: 50%;
+                width: calc(100vw - var(--scrollbar));
+                margin-left: calc(-50vw + calc(var(--scrollbar) / 2));
             }
-        @endforeach
-    @endif
-</style>
+        @endif
+        @if ($cardStyles)
+            @foreach ($cardStyles as $cardId => $values)
+                .{{ $cardClass }}-{{ $cardId }},
+                .{{ $cardClass }}-{{ $cardId }}:hover {
+                    background: {{ $values['background'] }} !important;
+                    color: {{ $values['color'] }} !important;
+                }
+            @endforeach
+        @endif
+    </style>
+@endif
